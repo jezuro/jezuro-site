688cd93511c7578462c9944510ec0850c161aad3
jezuro <jezuromusic@gmail.com>
2026-01-16 23:37:47 -0500

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,130 @@
+$(window).ready(() => {
+
+   /* let year = new Date().getFullYear();
+    $("#year").text(year);
+    $("#year-desk").text(year);*/
+    // for owl carousel
+
+    $(".music-carousel .owl-carousel").owlCarousel({
+        //loop: true,
+        margin: 10,
+        slideSpeed: 300,
+        smartSpeed: 1000,
+        paginationSpeed: 500,
+        autoHeight: true,
+        responsiveClass: true,
+        nav: true,
+        dots: false,
+
+        responsive: {
+            0: {
+                items: 1.5,
+                center: true
+            },
+            640: {
+                items: 1.5,
+                center: true
+            },
+            1024: {
+                items: 3.5,
+                margin: 25                
+            }
+        }
+    });
+
+    $(".video-carousel .owl-carousel").owlCarousel({
+        loop: true,
+        slideSpeed: 300,
+        smartSpeed: 1000,
+        paginationSpeed: 500,
+        responsiveClass: true,
+
+        responsive: {
+            0: {
+                margin: 10,
+                items: 1,
+                nav: true,
+                dots: false,
+            },
+            768: {
+                margin: 39,
+                items: 1,
+                nav: true,
+                dots: false,
+            }
+        }
+    });
+});
+
+var ytPlayers = [];
+var player;
+
+function playVideo(event) {
+    event.target.playVideo();
+}
+
+function playVideo(event) {
+    if (Object.keys(ytPlayers).length > 0) {
+        for (var k = 0; k < Object.keys(ytPlayers).length; k++) {
+            ytPlayers[Object.keys(ytPlayers)[k]].pauseVideo()
+        }
+    }
+    event.target.playVideo();
+}
+
+jQuery(document).ready(function () {
+
+
+    /*video script start*/
+    var count = 0;
+    jQuery(".video-carousel li").each(function () {
+        jQuery(this).find('.videowrap').click(function (event) {
+            event.preventDefault();
+            jQuery(this).parents(".item").find(".overlay-img").hide();
+            // jQuery(this).parents(".item").find(".gradient").hide();
+            var youtubeID = jQuery(this).parents(".item").find('.videoPlayer').attr('data-video-id');
+            count++;
+
+            jQuery(this).parents(".item").find('.videoPlayer').attr('id', 'ytplayer1' + count);
+            var playerID = jQuery(this).parents(".item").find('.videoPlayer').attr('id');
+            if (youtubeID.length > 11) {
+                formYoutubePlaylist(playerID, youtubeID);
+            } else {
+                formYoutubePlayer(playerID, youtubeID);
+            }
+            console.log("step4");
+        });
+    });
+
+});
+
+function formYoutubePlayer(playerID, youtubeID) {
+    player = new YT.Player(playerID, {
+        height: '390',
+        width: '340',
+        videoId: youtubeID,
+        host: 'https://www.youtube-nocookie.com',
+        events: {
+            'onReady': playVideo,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+    ytPlayers[playerID] = player;
+}
+
+function formYoutubePlaylist(playerID, youtubeID) {
+    player = new YT.Player(playerID, {
+        height: '390',
+        width: '340',
+        host: 'https://www.youtube-nocookie.com',
+        playerVars: {
+            listType: 'playlist',
+            list: youtubeID,
+        },
+        events: {
+            'onReady': playVideo,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+    ytPlayers[playerID] = player;
+}
---
+++ index.md
@@ -0,0 +1,8 @@
+---
+layout: default
+title: "The Official Website of Jezuro"
+---
+
+## You're ready to go!
+
+Start developing your Jekyll website.
