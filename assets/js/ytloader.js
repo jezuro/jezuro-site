7460a666d2549cafc8170386653896731d8c3edf
jezuro <jezuromusic@gmail.com>
2026-01-18 19:05:09 -0500

    Update ytloader.js

--- assets/js/ytloader.js
+++ assets/js/ytloader.js
@@ -25,7 +25,7 @@ player = new YT.Player('player', {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-event.target.playVideo();
+    event.target.playVideo();
 }
 
 // 5. The API calls this function when the player's state changes.
@@ -34,10 +34,10 @@ event.target.playVideo();
 var done = false;
 function onPlayerStateChange(event) {
 if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
+    //setTimeout(stopVideo, 6000);
+    //done = true;
 }
 }
 function stopVideo() {
-player.stopVideo();
+    player.stopVideo();
 }
